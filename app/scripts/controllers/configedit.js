f802efb7fa4254623a3123d2d16b8639b086ec1d
Eduardo Velez <edvelez1@gmail.com>
2015-02-04 14:25:15 -0500

    Hackathon

--- app/scripts/controllers/configedit.js
+++ app/scripts/controllers/configedit.js
@@ -9,31 +9,53 @@
  */
 angular.module('rwConfigGeneratorApp')
   .controller('ConfigeditCtrl', function ($scope, $http) {
-    $scope.content = [
-      {
-        title: 'foo',
-        indicator: [
-          {
-            "figure": 40
-          }
-        ]
-      },
-      {
-        title: 'bar'
-      }
-    ];
+    $scope.config = {
+        title: '',
+        dataSource: '',
+        map: {},
+        content: [],
+        indicator: [],
+
+      };
+
+
+    $scope.addContent = function() {
+      $scope.config.content.push({});
+    }
+    $scope.addIndicator = function() {
+      $scope.config.indicator.push({data:[{}]});
+    }
+    $scope.addData = function(index) {
+      console.log($scope.config.indicator, index)
+      $scope.config.indicator[index].data.push({});
+    }
+
+    $scope.addContent();
+    $scope.addIndicator();
 
     $scope.submit = function() {
-      console.log($scope.content);
+      var content = {}
+
+      for (var i in $scope.config.content) {
+        content[camelize($scope.config.content[i].title)] = $scope.config.content[i];
+      }
+      $scope.config.content = content;
       $http.post('https://api.github.com/gists', {
-        "files": {
-          "config.json": {
-            "content": JSON.stringify($scope.content)
+        'files': {
+          'config.json': {
+            'content': JSON.stringify($scope.config)
           }
         }
       }).
         success(function(data, status, headers) {
+          alert(data.html_url)
           console.log("Data", data);
         });
     };
+
+    function camelize(str) {
+      return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+        return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
+      }).replace(/\s+/g, '');
+    }
   });
